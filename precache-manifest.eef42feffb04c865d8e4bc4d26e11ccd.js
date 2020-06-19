a8a8c3e3272de1d06d177caf5ccdea3d1960bd34
chanjunweimy <chanjunweimy@gmail.com>
2020-06-19 20:16:32 +0800

    static web page

---
+++ precache-manifest.eef42feffb04c865d8e4bc4d26e11ccd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6e37a2067a544eb851b7d1f0f49153d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "42af741f287dc700d4b4",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7138753d392bac358162",
+    "url": "/static/js/2.64c5620e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.64c5620e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42af741f287dc700d4b4",
+    "url": "/static/js/main.5ec6c2b9.chunk.js"
+  },
+  {
+    "revision": "29ffb2c195ecc905db7a",
+    "url": "/static/js/runtime-main.425c2e9f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
